38f21f3ccac74b19bfead6657e642bc1c15d77b9
17146 <1714673995@qq.com>
2024-11-12 20:44:23 +0800

    添加BE基本栏位

--- src/main/java/cn/jacksigxu/min3halla/block/entity/DrinkMixerBlockEntity.java
+++ src/main/java/cn/jacksigxu/min3halla/block/entity/DrinkMixerBlockEntity.java
@@ -3,23 +3,138 @@ package cn.jacksigxu.min3halla.block.entity;
 import cn.jacksigxu.min3halla.init.MHBlockEntityTypes;
 import cn.jacksigxu.min3halla.recipe.MixingRecipe;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.SimpleContainer;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.wrapper.SidedInvWrapper;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
-public class DrinkMixerBlockEntity extends BlockEntity implements MenuProvider {
+public class DrinkMixerBlockEntity extends BlockEntity implements WorldlyContainer, MenuProvider {
+
+    protected static final int SLOT_SHAKER_POT = 0;
+    protected static final int SLOT_ADELHYDE = 1;
+    protected static final int SLOT_BRONSON_EXTRACT = 2;
+    protected static final int POWDERED_DELTA = 3;
+    protected static final int SLOT_FLANERGIDE = 4;
+    protected static final int SLOT_KARMOTRINE = 5;
+    protected static final int SLOT_ICE = 6;
+    protected static final int SLOT_AGED = 7;
+    protected static final int SLOT_DYE = 8;
+    protected static final int SLOT_EXTRA = 9;
+
+    private static final int[] SLOTS_FOR_UP = new int[]{0, 1, 2, 3, 4, 5};
+    private static final int[] SLOTS_FOR_SIDES = new int[]{6, 7, 8, 9};
+    private static final int[] SLOTS_FOR_DOWN = new int[]{0};
+
+    protected NonNullList<ItemStack> items = NonNullList.withSize(10, ItemStack.EMPTY);
+
+    private LazyOptional<?>[] itemHandlers = SidedInvWrapper.create(this, Direction.UP, Direction.DOWN, Direction.NORTH);
 
     public DrinkMixerBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(MHBlockEntityTypes.DRINK_MIXER_BLOCK_ENTITY.get(), pPos, pBlockState);
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        ContainerHelper.loadAllItems(pTag, this.items);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+        ContainerHelper.saveAllItems(pTag, this.items);
+    }
+
+    @Override
+    public int[] getSlotsForFace(Direction pSide) {
+        if (pSide == Direction.UP) {
+            return SLOTS_FOR_UP;
+        } else if (pSide == Direction.DOWN) {
+            return SLOTS_FOR_DOWN;
+        } else {
+            return SLOTS_FOR_SIDES;
+        }
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int pIndex, ItemStack pItemStack, @Nullable Direction pDirection) {
+        return pDirection != Direction.DOWN;
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int pIndex, ItemStack pStack, Direction pDirection) {
+        return pDirection == Direction.DOWN && pIndex == SLOT_SHAKER_POT;
+    }
+
+    @Override
+    public int getContainerSize() {
+        return this.items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (ItemStack itemstack : this.items) {
+            if (!itemstack.isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public ItemStack getItem(int pSlot) {
+        return this.items.get(pSlot);
+    }
+
+    @Override
+    public ItemStack removeItem(int pSlot, int pAmount) {
+        return ContainerHelper.removeItem(this.items, pSlot, pAmount);
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int pSlot) {
+        return ContainerHelper.takeItem(this.items, pSlot);
+    }
+
+    @Override
+    public void setItem(int pSlot, ItemStack pStack) {
+        ItemStack itemstack = this.items.get(pSlot);
+        boolean flag = !pStack.isEmpty() && ItemStack.isSameItemSameTags(itemstack, pStack);
+        this.items.set(pSlot, pStack);
+        if (pStack.getCount() > this.getMaxStackSize()) {
+            pStack.setCount(this.getMaxStackSize());
+        }
+
+        if (pSlot == 0 && !flag) {
+            this.setChanged();
+        }
+    }
+
+    @Override
+    public boolean stillValid(Player pPlayer) {
+        return Container.stillValidBlockEntity(this, pPlayer);
+    }
+
+    @Override
+    public void clearContent() {
+        this.items.clear();
     }
 
     private Optional<MixingRecipe> getCurrentRecipe() {
@@ -58,5 +173,43 @@ public class DrinkMixerBlockEntity extends BlockEntity implements MenuProvider {
     @Override
     public AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
         return null;
+    }
+
+    @Override
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag() {
+        CompoundTag compoundtag = new CompoundTag();
+        ContainerHelper.saveAllItems(compoundtag, this.items, true);
+        return compoundtag;
+    }
+
+    @Override
+    public <T> LazyOptional<T> getCapability(Capability<T> cap, @Nullable Direction side) {
+        if (!this.remove && side != null && cap == ForgeCapabilities.ITEM_HANDLER) {
+            if (side == Direction.UP) {
+                return itemHandlers[0].cast();
+            } else if (side == Direction.DOWN) {
+                return itemHandlers[1].cast();
+            } else {
+                return itemHandlers[2].cast();
+            }
+        }
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        for (LazyOptional<?> itemHandler : itemHandlers) itemHandler.invalidate();
+    }
+
+    @Override
+    public void reviveCaps() {
+        super.reviveCaps();
+        this.itemHandlers = SidedInvWrapper.create(this, Direction.UP, Direction.DOWN, Direction.NORTH);
     }
 }
