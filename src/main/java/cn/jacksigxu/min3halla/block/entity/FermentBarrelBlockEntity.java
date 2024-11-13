14ff0ee1feb0320b408ca071442992cebbed473c
Sherlock <2918789006@qq.com>
2024-11-14 01:56:00 +0800

    添加发酵桶BE功能

--- src/main/java/cn/jacksigxu/min3halla/block/entity/FermentBarrelBlockEntity.java
+++ src/main/java/cn/jacksigxu/min3halla/block/entity/FermentBarrelBlockEntity.java
@@ -1,30 +1,53 @@
 package cn.jacksigxu.min3halla.block.entity;
 
 import cn.jacksigxu.min3halla.init.MHBlockEntityTypes;
+import cn.jacksigxu.min3halla.recipe.FermentingRecipe;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.Container;
-import net.minecraft.world.ContainerHelper;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.wrapper.SidedInvWrapper;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
+
 public class FermentBarrelBlockEntity extends BlockEntity implements WorldlyContainer, MenuProvider {
 
     protected static final int SLOT_INPUT = 0;
     protected static final int SLOT_OUTPUT = 1;
 
+    public static final int MAX_PROGRESS = 1800;
+    public static final int MAX_DATA_COUNT = 1;
+
     protected NonNullList<ItemStack> items = NonNullList.withSize(2, ItemStack.EMPTY);
+
+    private int progress = 0;
+
+    protected final ContainerData dataAccess = new ContainerData() {
+        public int get(int pIndex) {
+            return FermentBarrelBlockEntity.this.progress;
+        }
+
+        public void set(int pIndex, int pValue) {
+            FermentBarrelBlockEntity.this.progress = pValue;
+        }
+
+        public int getCount() {
+            return MAX_DATA_COUNT;
+        }
+    };
 
     private LazyOptional<?>[] itemHandlers = SidedInvWrapper.create(this, Direction.UP, Direction.DOWN, Direction.NORTH);
 
@@ -32,6 +55,32 @@ public class FermentBarrelBlockEntity extends BlockEntity implements WorldlyCont
         super(MHBlockEntityTypes.FERMENT_BARREL_BLOCK_ENTITY.get(), pPos, pState);
     }
 
+    public static void serverTick(Level pLevel, BlockPos pPos, BlockState pState, FermentBarrelBlockEntity blockEntity) {
+        if (blockEntity.hasRecipe()) {
+            blockEntity.progress++;
+            if (blockEntity.progress >= MAX_PROGRESS) {
+                blockEntity.resetProgress();
+                blockEntity.craftItem();
+            }
+        } else {
+            blockEntity.resetProgress();
+        }
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        ContainerHelper.loadAllItems(pTag, this.items);
+        this.progress = pTag.getInt("Progress");
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+        ContainerHelper.saveAllItems(pTag, this.items);
+        pTag.putInt("Progress", this.progress);
+    }
 
     @Override
     public int[] getSlotsForFace(Direction pSide) {
@@ -107,9 +156,64 @@ public class FermentBarrelBlockEntity extends BlockEntity implements WorldlyCont
         this.items.clear();
     }
 
+    public void craftItem() {
+        Optional<FermentingRecipe> recipe = getCurrentRecipe();
+        if (recipe.isEmpty()) {
+            return;
+        }
+        ItemStack result = recipe.get().getResultItem(null);
+
+        ItemStack input = this.items.get(SLOT_INPUT);
+        input.shrink(1);
+
+        ItemStack output = this.items.get(SLOT_OUTPUT);
+        this.items.set(SLOT_OUTPUT, new ItemStack(result.getItem(), output.getCount() + result.getCount()));
+
+        this.setChanged();
+    }
+
+    private Optional<FermentingRecipe> getCurrentRecipe() {
+        if (this.level == null) {
+            return Optional.empty();
+        }
+        SimpleContainer inventory = new SimpleContainer(this.items.size());
+        for (int i = 0; i < this.items.size(); i++) {
+            inventory.setItem(i, this.items.get(i));
+        }
+        return this.level.getRecipeManager().getRecipeFor(FermentingRecipe.Type.INSTANCE, inventory, level);
+    }
+
     @Override
     public Component getDisplayName() {
         return Component.translatable("menu.min3halla.ferment_barrel");
+    }
+
+    private boolean hasRecipe() {
+        Optional<FermentingRecipe> recipe = getCurrentRecipe();
+
+        if (recipe.isEmpty()) {
+            return false;
+        }
+
+        if (getLevel() == null) {
+            return false;
+        }
+
+        ItemStack result = recipe.get().getResultItem(getLevel().registryAccess());
+
+        return canInsertAmountIntoOutputSlot(result.getCount()) && canInsertItemIntoOutputSlot(result.getItem());
+    }
+
+    private boolean canInsertItemIntoOutputSlot(Item item) {
+        return this.items.get(SLOT_OUTPUT).isEmpty() || this.items.get(SLOT_OUTPUT).is(item);
+    }
+
+    private boolean canInsertAmountIntoOutputSlot(int count) {
+        return this.items.get(SLOT_OUTPUT).getCount() + count <= this.items.get(SLOT_OUTPUT).getMaxStackSize();
+    }
+
+    private void resetProgress() {
+        this.progress = 0;
     }
 
     @Nullable
