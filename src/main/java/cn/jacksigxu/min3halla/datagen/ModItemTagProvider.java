0348af16736357631f7315ceb44ac2ee69302c7c
17146 <1714673995@qq.com>
2024-11-14 23:25:10 +0800

    添加部分物品tag

--- src/main/java/cn/jacksigxu/min3halla/datagen/ModItemTagProvider.java
+++ src/main/java/cn/jacksigxu/min3halla/datagen/ModItemTagProvider.java
@@ -1,6 +1,8 @@
 package cn.jacksigxu.min3halla.datagen;
 
 import cn.jacksigxu.min3halla.MHMod;
+import cn.jacksigxu.min3halla.init.MHItems;
+import cn.jacksigxu.min3halla.init.MHTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
@@ -19,6 +21,16 @@ public class ModItemTagProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(MHTags.Items.FLAVOR_SWEET).add(MHItems.BLUE_FAIRY.get(), MHItems.SUGAR_RUSH.get());
+        this.tag(MHTags.Items.FLAVOR_SPICY).add(MHItems.BLEEDING_JANE.get(), MHItems.BLOOM_LIGHT.get());
+        this.tag(MHTags.Items.FLAVOR_SOUR).add(MHItems.BAD_TOUCH.get(), MHItems.ZEN_STAR.get());
+//        this.tag(MHTags.Items.FLAVOR_BITTER).add();
+        this.tag(MHTags.Items.FLAVOR_BUBBLY).add(MHItems.BEER.get());
 
+        this.tag(MHTags.Items.TYPE_CLASSIC).add(MHItems.BEER.get(), MHItems.BLEEDING_JANE.get());
+        this.tag(MHTags.Items.TYPE_CLASSY).add(MHItems.BAD_TOUCH.get(), MHItems.BRANDTINI.get());
+        this.tag(MHTags.Items.TYPE_GIRLY).add(MHItems.BLUE_FAIRY.get(), MHItems.SUGAR_RUSH.get());
+//        this.tag(MHTags.Items.TYPE_MANLY).add();
+        this.tag(MHTags.Items.TYPE_PROMO).add(MHItems.ZEN_STAR.get(), MHItems.BLOOM_LIGHT.get());
     }
 }
---
+++ src/main/java/cn/jacksigxu/min3halla/init/MHTags.java
@@ -0,0 +1,38 @@
+package cn.jacksigxu.min3halla.init;
+
+import cn.jacksigxu.min3halla.MHMod;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class MHTags {
+
+    public static class Blocks {
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(MHMod.loc(name));
+        }
+    }
+
+    public static class Items {
+
+        public static final TagKey<Item> FLAVOR_SWEET = tag("flavor/sweet");
+        public static final TagKey<Item> FLAVOR_SPICY = tag("flavor/spicy");
+        public static final TagKey<Item> FLAVOR_SOUR = tag("flavor/sour");
+        public static final TagKey<Item> FLAVOR_BITTER = tag("flavor/bitter");
+        public static final TagKey<Item> FLAVOR_BUBBLY = tag("flavor/bubbly");
+
+        public static final TagKey<Item> TYPE_CLASSIC = tag("type/classic");
+        public static final TagKey<Item> TYPE_CLASSY = tag("type/classy");
+        public static final TagKey<Item> TYPE_GIRLY = tag("type/girly");
+        public static final TagKey<Item> TYPE_MANLY = tag("type/manly");
+        public static final TagKey<Item> TYPE_PROMO = tag("type/promo");
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(MHMod.loc(name));
+        }
+    }
+
+}
