261088a06cbaed9110bae4b793c7df65cd2b21f0
17146 <1714673995@qq.com>
2024-11-16 20:43:28 +0800

    调整调饮台GUI

--- src/main/java/cn/jacksigxu/min3halla/compat/jei/MHJeiPlugin.java
+++ src/main/java/cn/jacksigxu/min3halla/compat/jei/MHJeiPlugin.java
@@ -42,7 +42,7 @@ public class MHJeiPlugin implements IModPlugin {
 
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
-        registration.addRecipeClickArea(DrinkMixerScreen.class, 217, 81, 16, 16, MixingCategory.TYPE);
+        registration.addRecipeClickArea(DrinkMixerScreen.class, 98, 88, 46, 13, MixingCategory.TYPE);
     }
 
     @Override
